e3989dc7fc818dc69a398b6cf9e161d80d3a2bc9
Jeffrey Tan <60846941+tanj85@users.noreply.github.com>
2023-11-20 19:48:02 -0500

    GameManager
    Added GameManager Script as a Singleton, and added the GameManager Prefab

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager _instance;
+	public static GameManager Instance { get { return _instance; } }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
